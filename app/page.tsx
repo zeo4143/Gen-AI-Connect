cbbe1c76759cf07e59e81fff765f76ea891b6d33
zeo <mahammadrafi53277@gmail.com>
2024-03-18 16:47:12 +0530

    Added button to main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,23 @@
-"use client"
+"use client";
 import { Button } from "@/components/ui/button";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import ChangeTheme from "@/components/theme/change-theme";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons"
+import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
 import { useTheme } from "next-themes";
+import Link from "next/link";
 
 export default function Home() {
-  const {setTheme} = useTheme()
+  const { setTheme } = useTheme();
   return (
-    <main>
-     <ChangeTheme/>
+    <main className="h-full flex items-center justify-center">
+      <Button>
+        <Link href="/dashboard">Dashboard</Link>
+      </Button>
     </main>
   );
 }
