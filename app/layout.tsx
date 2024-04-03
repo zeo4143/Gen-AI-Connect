602d2d1fe4de3bb1c1e269763bc0b181db452e5c
zeo <mahammadrafi53277@gmail.com>
2024-04-04 04:36:46 +0530

    uncommneted header

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {/* <Header/> */}
+          <Header/>
           {children}
           <Footer/>
         </ThemeProvider>
